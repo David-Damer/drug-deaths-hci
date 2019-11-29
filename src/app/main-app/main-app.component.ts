1edc915fddff8c5a2ba0509f6a987031a127cfbb
David Damer <david1975damer@gmail.com>
2019-11-29 16:43:24 +0000

    Some fixes and added an array of colours

--- src/app/main-app/main-app.component.ts
+++ src/app/main-app/main-app.component.ts
@@ -18,14 +18,9 @@ export class MainAppComponent implements AfterViewInit, OnInit {
 
   public barChartOptions: ChartOptions = {
     responsive: true,
+    maintainAspectRatio: false,
     // We use these empty structures as placeholders for dynamic theming.
     scales: {xAxes: [{}], yAxes: [{}]},
-    plugins: {
-      datalabels: {
-        anchor: 'end',
-        align: 'end',
-      }
-    }
   };
   public barChartData: ChartDataSets[] = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'}
@@ -33,7 +28,7 @@ export class MainAppComponent implements AfterViewInit, OnInit {
   public barChartLabels: Label[] = ['All', 'Heroin/morphine 2', 'Methadone', 'Heroin/Methadone/Buprenorphine', 'Codeine', 'Dihydrocodeine'
     , 'Opiates', 'Benzodiazepines', 'Gabapentin/Pregabalin', 'Cocaine', 'Ecstacy', 'Amphetamines', 'Alcohol'];
   public barChartType: ChartType = 'bar';
-  public barChartLegend = true;
+  public barChartLegend = false;
 
   private selectedOne: string;
   private selectedTwo: string;
@@ -46,6 +41,9 @@ export class MainAppComponent implements AfterViewInit, OnInit {
   private tableData;
   private displayedColumns: string[] = ['drugs', 'deaths'];
   private heading: string;
+  private colours = ['#000000', '#080000', '#100000', '#180000', '#200000', '#280000', '#300000', '#380000', '#400000', '#480000',
+    '#500000', '#580000', '#600000', '#680000', '#700000', '#780000', '#800000', '#880000', '#900000', '#980000', '#A00000', '#A80000',
+    '#B00000', '#B80000', '#C00000', '#C80000', '#D00000', '#D80000', '#E00000', '#E80000', '#F00000', '#F80000', '#FF0000'];
 
   private static highlightFeature(e) {
     const layer = e.target;
@@ -75,6 +73,7 @@ export class MainAppComponent implements AfterViewInit, OnInit {
         this.initialiseChartData();
       },
     );
+    console.log(this.colours.length);
   }
 
   private toggleDetails(): void {
@@ -103,12 +102,11 @@ export class MainAppComponent implements AfterViewInit, OnInit {
   private initialiseChartData() {
     this.barChartData.pop();
     for (const val of Object.keys(this.drugData)) {
-      const dataObj = {label: val, data: [], borderColor: 'red', fill: true};
+      const dataObj = {label: val, data: [], borderColor: '#008f68', fill: true, backgroundColor: '#008f68'};
       const data = [];
       for (const item of this.drugData[val]) {
         data.push(parseInt(item.deaths, 10));
       }
-      console.log(data);
       dataObj.data = data;
       this.barChartData.push(dataObj);
     }
